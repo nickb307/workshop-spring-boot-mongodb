f61311ea678cfce77add8e62139ed4c6b6d17849
nickb307 <nicoleb3007@gmail.com>
2023-06-25 20:34:27 -0300

    entity user

--- src/main/java/com/user/workshopmongo/WorkshopmongoApplication2.java
+++ src/main/java/com/user/workshopmongo/WorkshopmongoApplication2.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WorkshopmongoApplication {
+public class WorkshopmongoApplication2 {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WorkshopmongoApplication.class, args);
+		SpringApplication.run(WorkshopmongoApplication2.class, args);
 	}
 
 }
