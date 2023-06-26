968fd4d0394d31ae36fb569ecc0dbaae76cbc6c8
nickb307 <nicoleb3007@gmail.com>
2023-06-26 11:38:50 -0300

    obtendo usuario por id e tratando execções

--- src/main/java/com/user/workshopmongo/services/UserService.java
+++ src/main/java/com/user/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.user.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.user.workshopmongo.domain.User;
 import com.user.workshopmongo.repository.UserRepository;
+import com.user.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,6 +19,14 @@ public class UserService {
 	public List<User> findAll(){
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		User user =repo.findById(id).orElseThrow(
+	            () -> new ObjectNotFoundException("Usuário: " + id+ " não encontrado")
+		        ); 
+		return user;
+	}
+	
 	
 	
 }
